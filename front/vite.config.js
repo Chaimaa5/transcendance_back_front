b32d79a76a54abc33d2194fb8ae74e470411ece5
Chaimaa El mhandez <cel-mhan@e1r2p8.1337.ma>
2023-07-22 15:37:24 +0100

    nginx

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: '8000'
-  },
+    port: '3000',
+    hmr: {
+      clientPort: 8000,
+    },
+    },
   plugins: [react()],
 })
 
