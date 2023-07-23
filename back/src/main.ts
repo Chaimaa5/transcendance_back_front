9e18f08181d5e6550560f815432be17b499c554e
Chaimaa El mhandez <cel-mhan@e1r6p4.1337.ma>
2023-07-23 15:59:44 +0100

    nginx

--- back/src/main.ts
+++ back/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.useWebSocketAdapter(new IoAdapter(app));
   app.use(cors(corsOptions));
   app.use(cookieParser());
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
